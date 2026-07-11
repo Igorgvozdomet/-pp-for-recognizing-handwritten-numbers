dfc0be2bdde4e3430b9e9b4d20f46dbae869401d
Igor <146733542+Igorgvozdomet@users.noreply.github.com>
2026-07-11 14:16:40 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,18 @@
+# Размеры окна
+WINDOW_WIDTH = 450
+WINDOW_HEIGHT = 550
+
+# Размеры холста
+CANVAS_WIDTH = 400
+CANVAS_HEIGHT = 400
+
+# Настройки рисования
+BRUSH_SIZE = 15
+BRUSH_COLOR = "white"
+BG_COLOR = "black"
+
+# База данных
+DB_NAME = 'users.db'
+
+# Модель
+MODEL_PATH = 'mnist_cnn.keras'
